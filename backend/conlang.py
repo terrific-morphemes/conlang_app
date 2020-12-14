f027ac1f7ed345c788acfdd540d201683c21d62c
Nicholas Miller <nicholas.anthony.miller@gmail.com>
2020-12-13 21:35:01 -0500

    save conlang

--- backend/conlang.py
+++ backend/conlang.py
@@ -8,14 +8,16 @@ CFG_FNAME = Path("./backend/configs/tiefling_config.yaml")
 # CFG_FNAME = Path("./backend/configs/spritish_config.yaml")
 # CFG_FNAME = Path("./backend/configs/tonal_config.yaml")
 
+DEST_FNAME = Path("./backend/data/tiefling_conlang.json")
 # TODO: save to json
 
-
-ORD_SCALE = {
+PARAM_CONVERSION = {
     'none':0,
     'low':1,
     'med':2,
     'high':3,
+    'yes':True,
+    'no':False,
 }
 
 
@@ -54,7 +56,7 @@ def load_phonemes(phoneme_fname):
                 'xsampa':row['xsampa'],
                 'ipa':row['ipa'],
                 'description':row['description'],
-                'level':to_ord(row['level']),
+                'level':PARAM_CONVERSION.get(row['level'], row['level']),
             }
             all_phonemes.append(phoneme)
     return all_phonemes
@@ -105,15 +107,38 @@ def load_sentences(sentence_fname):
     return all_sentences
 
 
-def to_bool(string):
-    return True if string == 'yes' else False
-
-
-def to_ord(string):
-    return ORD_SCALE[string]
+def convert_param(params, param):
+    setting = params[param]
+    if type(setting) == list:
+        return setting
+    else:
+        return PARAM_CONVERSION.get(setting, setting)
 
 
 class Conlang:
+    def save_conlang(self, dest_fname):
+        data = {
+            'all_phonemes':self.all_phonemes,
+            'all_morphemes':self.all_morphemes,
+            'all_adpositions':self.all_adpositions,
+            'all_lexemes':self.all_lexemes,
+            'all_sentences':self.all_sentences,
+            'lexicon':self.lexicon,
+            'morphemes':self.morphemes,
+            'adpositions':self.adpositions,
+            'initials':self.initials,
+            'medials':self.medials,
+            'finals':self.finals,
+            'tones':self.tones,
+            'morphology_rules':self.morphology_rules,
+            'phonology_rules':self.phonology_rules,
+            'syntax_rules':self.syntax_rules,
+            'lexicon_rules':self.lexicon_rules,
+        }
+
+        with dest_fname.open('w') as dest:
+            json.dump(data, dest, indent=1)
+
     def create_phonology(self):
         all_vowels = [
             p for p in self.all_phonemes
@@ -127,29 +152,30 @@ class Conlang:
 
         vowels_matching_level = [
             v for v in all_vowels
-            if v['level'] <= self.phon_vowel_difficulty
+            if v['level'] <= self.phonology_rules['phon_vowel_difficulty']
         ]
 
         consonants_matching_level = [
             c for c in all_consonants
-            if c['level'] <= self.phon_cons_difficulty
+            if c['level'] <= self.phonology_rules['phon_cons_difficulty']
         ]
 
-        if self.phon_vowel_number == 0:
+        vowel_number = self.phonology_rules['phon_vowel_number']
+        if vowel_number == 0:
             n_vowels = 0
-        elif self.phon_vowel_number == 1:
+        elif vowel_number == 1:
             n_vowels = random.randint(5,8)
-        elif self.phon_vowel_number == 2:
+        elif vowel_number == 2:
             n_vowels = random.randint(8,12)
         else:
             n_vowels = random.randint(12, 20)
 
-
-        if self.phon_cons_number == 0:
+        cons_number = self.phonology_rules['phon_cons_number']
+        if cons_number == 0:
             n_consonants = 0
-        elif self.phon_cons_number == 1:
+        elif cons_number == 1:
             n_consonants = random.randint(5,10)
-        elif self.phon_cons_number == 2:
+        elif cons_number == 2:
             n_consonants = random.randint(10,15)
         else:
             n_consonants= random.randint(15, 30)
@@ -162,7 +188,7 @@ class Conlang:
 
         self.initials = consonants
         self.medials = vowels
-        if self.phon_syl_cons > 0:
+        if self.phonology_rules['phon_syl_cons'] > 0:
             syllabic_consonants = [
                 c for c in consonants
                 if 'nasal' in c['description']
@@ -171,11 +197,12 @@ class Conlang:
             ]
             self.medials.extend(syllabic_consonants)
         self.finals = consonants
-        if self.phon_tone >= 1:
+        tone = self.phonology_rules['phon_tone']
+        if tone >= 1:
             self.tones.extend(['55', '11'])
-        if self.phon_tone >= 2:
+        if tone >= 2:
             self.tones.extend(['51', '15'])
-        if self.phon_tone >= 3:
+        if tone >= 3:
             self.tones.extend(['33', '13', '31', '314', '53', '35'])
 
         # TODO: consonant clusters
@@ -199,9 +226,9 @@ class Conlang:
                     has_geminate_initial = False
                     has_geminate_final = False
                     has_long_vowel = False
-                    if self.phon_gemination:
+                    if self.phonology_rules['phon_cons_length']:
                         has_geminate_initial = random.random() >= .5
-                    if self.phon_vowel_length:
+                    if self.phonology_rules['phon_vowel_length']:
                         has_long_vowel = random.random() >= .5
                     if has_initial:
                         initial = random.choice(self.initials)['ipa']
@@ -263,8 +290,8 @@ class Conlang:
         for morpheme in morphemes:
             morpheme = self.create_lemma(
                 morpheme,
-                self.min_syl_per_morpheme,
-                self.max_syl_per_morpheme
+                self.phonology_rules['min_syl_per_morpheme'],
+                self.phonology_rules['max_syl_per_morpheme']
             )
             """
             print(
@@ -276,68 +303,114 @@ class Conlang:
 
 
     def create_morphology(self):
-        # TODO: copula
 
-        if self.verb_person > 0:
-            self.add_morphemes(
-                'verb_person',
-            )
+        for param, setting in self.morphology_rules.items():
+            if setting is True:
+                self.add_morphemes(param)
+            elif type(setting) == int and setting > 0:
+                self.add_morphemes(param)
+        verb_agreement = self.morphology_rules['verb_agreement']
+        verb_agree_with = self.morphology_rules['verb_agree_with']
+        if verb_agree_with != 'none' and verb_agree_with != 0:
+            verb_morphemes = set()
+            person = list()
+            number = list()
+            noun_class = list()
+            case = list()
+            if 'person' in verb_agreement:
+                person = [
+                    m['morpheme'] for category in self.morphemes 
+                    if 'noun_person' in m['category']
+                ]
+            if 'number' in verb_agreement:
+                number = [
+                    m['morpheme'] for category in self.morphemes 
+                    if 'noun_number' in m['category']
+                ]
+            if 'class' in verb_agreement:
+                noun_class = [
+                    m['morpheme'] for category in self.morphemes 
+                    if 'noun_class' in m['category']
+                ]
+            if 'case' in verb_agreement:
+                case = [
+                    m['morpheme'] for category in self.morphemes 
+                    if 'noun_case' in m['category']
+                ]
+            if not person:
+                person = [""]
+            if not number:
+                number = [""]
+            if not noun_class:
+                noun_class = [""]
+            if not case:
+                case = [""]
+            for p in person:
+                for n in number:
+                    for g in noun_class:
+                        for c in case:
+                            verb_morpheme = f"{p}{n}{g}{c}"
+                            verb_morphemes.add(verb_morpheme)
+            for verb_morpheme in verb_morphemes:
+                lexeme = {
+                    'category':'verb_agreement',
+                    'lemma':'',
+                    'morpheme':f'verb_{verb_morpheme}'
+                }
+                lexeme = self.create_lemma(
+                    lexeme,
+                    self.phonology_rules['min_syl_per_morpheme'],
+                    self.phonology_rules['max_syl_per_morpheme']
+                )
+                self.morphemes.append(lexeme)
+        
+        if self.morphology_rules['pronouns'] is True:
+            pronoun_agreement = self.morphology_rules['pronoun_agreement']
+            pronouns = set()
+            person = list()
+            number = list()
+            noun_class = list()
+            case = list()
 
-        if self.verb_number > 0:
-            self.add_morphemes(
-                'verb_number',
-            )
-
-        if self.verb_gender > 0:
-            self.add_morphemes(
-                'verb_gender',
-            )
-
-        if self.verb_tense > 0:
-            self.add_morphemes(
-                'verb_tense',
-            )
-
-        if self.verb_aspect > 0:
-            self.add_morphemes(
-                'verb_aspect',
-            )
-
-        if self.verb_mood > 0:
-            self.add_morphemes(
-                'verb_mood',
-            )
-
-        if self.verb_attitudinal > 0:
-            self.add_morphemes(
-                'verb_attitudinal',
-            )
-
-        if self.verb_evidential > 0:
-            self.add_morphemes(
-                'verb_evidential',
-            )
-
-        if self.derivational > 0:
-            self.add_morphemes(
-                'derivational',
-            )
-
-        if self.verb_valence > 0:
-            self.add_morphemes(
-                'verb_valence',
-            )
-
-        if self.noun_class > 0:
-            self.add_morphemes(
-                'noun_class',
-            )
-
-        if self.noun_case > 0:
-            self.add_morphemes(
-                'noun_case',
-            )
-
+            if 'person' in pronoun_agreement:
+                person = [
+                    m['meaning'] for m in self.morphemes 
+                    if 'noun_person' in m['category']
+                ]
+            if 'number' in pronoun_agreement:
+                number = [
+                    m['meaning'] for m in self.morphemes 
+                    if 'noun_number' in m['category']
+                ]
+            if 'class' in pronoun_agreement:
+                noun_class = [
+                    m['meaning'] for m in self.morphemes 
+                    if 'noun_class' in m['category']
+                ]
+            if not person:
+                person = [""]
+            if not number:
+                number = [""]
+            if not noun_class:
+                noun_class = [""]
+            for p in person:
+                for n in number:
+                    for c in noun_class:
+                        pronoun = f"{p}{n}{c}"
+                        pronouns.add(pronoun)
+            for pronoun in pronouns:
+                lexeme = {
+                    'category':'pronouns',
+                    'lemma':'',
+                    'morpheme':f'pronoun_{pronoun}'
+                }
+                lexeme = self.create_lemma(
+                    lexeme,
+                    self.phonology_rules['min_syl_per_morpheme'],
+                    self.phonology_rules['max_syl_per_morpheme']
+                )
+                self.morphemes.append(lexeme)
+         # TODO: inflection classes
 
     def generate_sentence(self):
         n_subject = dict()
@@ -345,7 +418,7 @@ class Conlang:
         verb = dict()
         tense = dict()
         aspect = dict()
-        mood = dict()
+        modality = dict()
         evidential = dict()
         place = dict()
         locative = dict()
@@ -384,9 +457,9 @@ class Conlang:
             if 'aspect' in m['category']
         ]
 
-        moods = [
+        modalities = [
             m for m in self.morphemes
-            if 'mood' in m['category']
+            if 'modality' in m['category']
         ]
 
         evidentials = [
@@ -420,8 +493,8 @@ class Conlang:
             tense = random.choice(tenses)
         if aspects:
             aspect = random.choice(aspects)
-        if moods:
-            mood = random.choice(moods)
+        if modalities:
+            modality = random.choice(modalities)
         if evidentials:
             evidential = random.choice(evidentials)
 
@@ -431,7 +504,7 @@ class Conlang:
             'verb':verb,
             'tense':tense,
             'aspect':aspect,
-            'mood':mood,
+            'modality':modality,
             'place':place,
             'locative':locative,
             'evidential':evidential,
@@ -444,15 +517,15 @@ class Conlang:
             if self.adpositions == 'morpheme':
                 adposition = self.create_lemma(
                     adposition,
-                    self.min_syl_per_morpheme,
-                    self.max_syl_per_morpheme
+                    self.phonology_rules['min_syl_per_morpheme'],
+                    self.phonology_rules['max_syl_per_morpheme']
                 )
                 self.morphemes.append(adposition)
             else:
                 adposition = self.create_lemma(
                     adposition,
-                    self.min_syl_per_word,
-                    self.max_syl_per_word
+                    self.phonology_rules['min_syl_per_word'],
+                    self.phonology_rules['max_syl_per_word']
                 )
                 self.lexicon.append(adposition)
 
@@ -460,8 +533,8 @@ class Conlang:
         for lexeme in self.all_lexemes:
             lexeme = self.create_lemma(
                 lexeme,
-                self.min_syl_per_word,
-                self.max_syl_per_word
+                self.phonology_rules['min_syl_per_word'],
+                self.phonology_rules['max_syl_per_word']
             )
             self.lexicon.append(lexeme)
 
@@ -469,7 +542,11 @@ class Conlang:
     def __init__(self, cfg_fname):
         cfg = load_config(cfg_fname)
         data_cfg = cfg['data']
-        params_cfg = cfg['params']
+        phonology_cfg = cfg['phonology']
+        morphology_cfg = cfg['morphology']
+        lexicon_cfg = cfg['lexicon']
+        syntax_cfg = cfg['syntax']
+
         lexicon_fnames = [Path(f) for f in data_cfg['lexicon_fnames']]
         adposition_fname = Path(data_cfg['adposition_fname'])
         phoneme_fname = Path(data_cfg['phoneme_fname'])
@@ -492,55 +569,28 @@ class Conlang:
         self.medials = list()
         self.finals = list()
         self.tones = list()
+        self.morphology_rules = dict()
+        self.phonology_rules = dict()
+        self.syntax_rules = dict()
+        self.lexicon_rules = dict()
 
-        self.min_syl_per_word = int(params_cfg['min_syl_per_word'])
-        self.max_syl_per_word = int(params_cfg['min_syl_per_word'])
-
-        self.min_syl_per_morpheme = int(params_cfg['min_syl_per_morpheme'])
-        self.max_syl_per_morpheme = int(params_cfg['max_syl_per_morpheme'])
-
-        self.onomatopoeia = params_cfg['onomatopoeia']
-        self.phon_cons_difficulty = to_ord(params_cfg['phon_cons_difficulty'])
-        self.phon_cons_number = to_ord(params_cfg['phon_cons_number'])
-        self.phon_vowel_difficulty = to_ord(params_cfg['phon_vowel_difficulty'])
-        self.phon_vowel_number = to_ord(params_cfg['phon_vowel_number'])
-
-        self.phon_syl_cons = to_ord(params_cfg['phon_syl_cons'])
-        self.phon_tone = to_ord(params_cfg['phon_tone'])
-        self.phon_gemination = to_bool(params_cfg['phon_gemination'])
-        self.phon_vowel_length =to_bool( params_cfg['phon_vowel_length'])
-        self.phon_non_egressive = to_bool(params_cfg['phon_non_egressive'])
-        self.phon_initial_clusters = to_ord(params_cfg['phon_initial_clusters'])
-        self.phon_final_clusters = to_ord(params_cfg['phon_final_clusters'])
-        self.phon_diphthongs = to_ord(params_cfg['phon_diphthongs'])
+        for param in phonology_cfg:
+            self.phonology_rules[param] = convert_param(phonology_cfg, param)
+        for param in syntax_cfg:
+            self.syntax_rules[param] = convert_param(syntax_cfg, param)
+        for param in morphology_cfg:
+            self.morphology_rules[param] = convert_param(morphology_cfg, param)
+        for param in lexicon_cfg:
+            self.lexicon_rules[param] = convert_param(lexicon_cfg, param)
 
         self.create_phonology()
-
-        self.word_order = params_cfg['word_order']
-        self.noun_class = to_ord(params_cfg['noun_class'])
-        self.noun_case = to_ord(params_cfg['noun_case'])
-        self.pronouns = to_ord(params_cfg['pronouns'])
-        self.adpositions = params_cfg['adpositions']
-
-        self.verb_copula = to_bool(params_cfg['verb_copula'])
-        self.verb_person = to_ord(params_cfg['verb_person'])
-        self.verb_number = to_ord(params_cfg['verb_number'])
-        self.verb_gender = to_ord(params_cfg['verb_gender'])
-        self.verb_tense = to_ord(params_cfg['verb_tense'])
-        self.verb_aspect = to_ord(params_cfg['verb_aspect'])
-        self.verb_mood = to_ord(params_cfg['verb_mood'])
-        self.verb_attitudinal = to_ord(params_cfg['verb_attitudinal'])
-        self.verb_evidential = to_ord(params_cfg['verb_evidential'])
-        self.derivational = to_ord(params_cfg['derivational'])
-        self.verb_valence = to_ord(params_cfg['verb_valence'])
-
         self.create_morphology()
         self.create_adpositions()
-
         self.build_lexicon()
 
 if __name__ == "__main__":
     conlang = Conlang(CFG_FNAME)
+    conlang.save_conlang(DEST_FNAME)
     """
     for lexeme in conlang.lexicon:
         meaning = lexeme['meaning']
@@ -549,7 +599,7 @@ if __name__ == "__main__":
     """
     for i in range(20):
         sent = conlang.generate_sentence()
-        for constituent in conlang.word_order:
+        for constituent in conlang.syntax_rules['word_order']:
             if constituent == 's':
                 lemma = sent['subject']['lemma']
                 meaning = sent['subject']['meaning']
